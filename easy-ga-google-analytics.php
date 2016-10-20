1125e6cfad0d972d36343c11abb49bd3c8e91671
Jose Antonio Sabalete <joseantonio.sabalete@extern.isban.com>
2016-10-20 19:17:21 +0200

    Esqueleto
    Primara subida con la información básica del plugin.

---
+++ easy-ga-google-analytics.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name: Easy GA Google Analytics
+ * Plugin URI: http://www.programaconsaba.com/easy-ga-google-analytics
+ * Description: Copy&Paste your Google Analytics code snippet and enables google analytics on all pages. Only Anonymous users have analytics.
+ * Version: 1.0.0-SNAPSHOT
+ * Author: Jose Antonio Sabalete
+ * Author URI: http://www.programaconsaba.com/quienes-somos/
+ * License: GNU Affero General Public License v3.0
+ * License URI: https://www.gnu.org/licenses/agpl-3.0.html
+ * Text Domain: easy-ga
+ * Domain Path: /languages
+ * Tags: google analytics, analytics, ga, google 
+ */
