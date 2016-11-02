ff7a9d25f3b9c438a118c41b60c2487320e2f107
Jose Antonio Sabalete <joseantonio.sabalete@extern.isban.com>
2016-11-02 17:59:29 +0100

    Tercer requisito
    Si el usuario es un usuario con rol Administrador, NO debe aparecer el
    código de analítica para que no se registre la visita.

--- easy-ga-google-analytics.php
+++ easy-ga-google-analytics.php
@@ -3,7 +3,7 @@
  * Plugin Name: Easy GA Google Analytics
  * Plugin URI: http://www.programaconsaba.com/easy-ga-google-analytics
  * Description: Copy&Paste your Google Analytics code snippet and enables google analytics on all pages. Only Anonymous users have analytics.
- * Version: 1.0.0-SNAPSHOT
+ * Version: 1.0.0
  * Author: Jose Antonio Sabalete
  * Author URI: http://www.programaconsaba.com/quienes-somos/
  * License: GNU Affero General Public License v3.0
@@ -32,8 +32,25 @@ function easy_ga_load_page () {
 // Se utiliza el hook admin_menu para añadir menús y submenús extra en el panel de administración.
 add_action( 'admin_menu', 'easy_ga_init' );
 
+/**
+ * Esta función escribe el script de Google Analytics guardado en Wordpress y lo coloca en la cabecera. 
+ * Si el usuaior logado es un usuario con perfil administrador, el script no se añade por lo tanto la 
+ * analítica no registra la visita.
+ */
 function easy_ga_custom() {
-	echo stripslashes(get_option ( 'easy-ga-script-option' ) );
+    global $current_user;
+    $isAdmin = false;
+
+    $user_roles = $current_user->roles;
+    $user_role = array_shift($user_roles);
+
+    if($user_role === "administrator"){
+        $isAdmin = true;
+    }
+
+    if (!$isAdmin){
+		echo stripslashes(get_option ( 'easy-ga-script-option' ) );
+    }
 }
 
 // Se utiliza el hook wp_head para insertar código en las etiquetas <head></head> de la plantilla del usuario
